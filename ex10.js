960e2ddd4d5f767b09c62d1767a3a096301cca2c
JUNG JUN YONG <jkouese@gmail.com>
2020-04-07 11:05:44 +0900

    ex예제

--- ex10.js
+++ ex10.js
@@ -1,3 +1,5 @@
+var Rect = require('./rect.js');
+var Rect2 = Rect.Rect();
 window.addEventListener('load',function(){
 	var rects = [new Rect(100,100, 200,300,"#00f"),new Rect(250,250, 400,400,"#0f0"),new Rect(400,400, 500,600,"#f00")];
 	var index=0;
